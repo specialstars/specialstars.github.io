2cf77b3029cf6706fc2aa004831c7850f7e76c97
saadalamin <saadalamin2004@gmail.com>
2023-09-14 11:58:38 +0600

    .

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,69 @@
-var lazyLoad = new LazyLoad({
-  elements_selector: ".lazy",
-  callback_error: (img) => {
-    img.setAttribute("src", "https://placehold.co/600x400?text=Error");
-  },
-});
+"use strict";
+(function (window) {
+  const $$$ = function () {
+    /*------------------------------------------------
+  Global
+  -
+  @ Navbar Scroll Effect
+  */
+    /////
+    const $sc = function () {
+      let header = document.querySelectorAll(".__navbar")[0];
+      let headerHeight = header.offsetHeight;
+      let scrollTop = window.pageYOffset || window.scrollTop;
+      if (scrollTop > headerHeight + 100) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    };
+    window.addEventListener("scroll", $sc);
+    window.addEventListener("load", $sc);
+
+    /*------------------------------------------------
+  Home Page Interaction
+  -
+  @ Search box toggle
+  @ Animation on Scroll
+  ------------------------------------------------
+  */
+
+    //////
+
+    /////
+
+    /*------------------------------------------------
+  Team Page Interaction
+  -
+  @ Lazy Image Loading
+  ------------------------------------------------
+  */
+
+    /////
+
+    var lazyLoad = new LazyLoad({
+      elements_selector: ".lazy",
+      callback_error: (img) => {
+        img.setAttribute("src", "https://placehold.co/600x400?text=Error");
+      },
+    });
+
+    /////
+
+    /*------------------------------------------------
+  Footer Section Interaction
+  -
+  @ Copyright Year Update
+  ------------------------------------------------
+  */
+
+    /////
+
+    let cpY = document.getElementById("copyrightYear");
+    cpY.innerHTML = new Date().getFullYear();
+
+    /////
+  };
+  window.addEventListener("load", $$$);
+  document.addEventListener("DOMContentLoaded", $$$);
+})(window || {});
