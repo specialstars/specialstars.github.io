17b656327cc4464cd8a8b8661796eb57ea1aa233
mosefatullah <mosefatullah@gmail.com>
2023-09-16 21:01:16 +0600

    .

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -22,33 +22,30 @@
   /////
   const $fade = function () {
    let elementsArray = [
-    document.querySelectorAll(".__home-page .__b-section .__card"),
+    ...document.querySelectorAll(
+     ".__team-page .__list-section .__lists .__list"
+    ),
+    ...document.querySelectorAll(".__home-page .__b-section .__card"),
     document.querySelector(".__home-page .__c-section"),
     document.querySelector(".__home-page .__faq-section"),
-    document.querySelector(".__team-page .__list-section .__lists"),
    ];
 
    function fadeIn(elem) {
     var distInView = elem.getBoundingClientRect().top - window.innerHeight;
-    elem.classList.add("__animation");
-    if (distInView < 0) {
-     elem.classList.add("active");
-    } else {
-     elem.classList.remove("active");
+    if (distInView) {
+     elem.classList.add("__animation");
+     if (distInView < 0) {
+      elem.classList.add("active");
+     } else {
+      elem.classList.remove("active");
+     }
     }
    }
 
    window.addEventListener("scroll", function () {
     for (var i = 0; i < elementsArray.length; i++) {
-     if (elementsArray[i] == undefined) continue;
-     if (elementsArray[i].length > 1) {
-      if (elementsArray[i][0] == undefined) continue;
-      for (var j = 0; j < elementsArray[i].length; j++) {
-       fadeIn(elementsArray[i][j]);
-      }
-     } else {
-      fadeIn(elementsArray[i]);
-     }
+     if (typeof elementsArray[i] == "undefined") continue;
+     fadeIn(elementsArray[i]);
     }
    });
   };
