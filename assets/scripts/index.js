03cc34f57b6423c7cf944fd01718da3f898a8e2a
mosefatullah <mosefatullah@gmail.com>
2023-09-17 12:45:41 +0600

    .

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -50,6 +50,22 @@
    });
   };
   /////
+
+  // Start of Tawk.to Script
+  var Tawk_API = Tawk_API || {},
+   Tawk_LoadStart = new Date();
+  (function () {
+   var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+   s1.async = true;
+   s1.src = "https://embed.tawk.to/65069f500f2b18434fd8f3e9/1hagtslgt";
+   s1.charset = "UTF-8";
+   s1.setAttribute("crossorigin", "*");
+   s0.parentNode.insertBefore(s1, s0);
+  })();
+  // End of Tawk.to Script
+
+  /////
   const $main = function () {
    $scroll();
    $fade();
