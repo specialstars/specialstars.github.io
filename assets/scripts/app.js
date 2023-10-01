be2c52ac21257d81d8e8360c3af4fa34524f30a9
mosefatullah <mosefatullah@gmail.com>
2023-10-01 13:05:47 +0600

    .

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -119,9 +119,7 @@ function createPostElement(i, data) {
             </div>
             <div class="__body col-md-6 ${data.type == "event" && `col-md-12`}">
                 <h5>${data.type == "event"
-      ? `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-calendar-event-fill me-2 mb-1" viewBox="0 0 16 16">
-                <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>
-              </svg>`
+      ? `📣`
       : ""
     } ${title + (data.type == "event" ? " (" + data.tid + ")" : "")
     }</h5>
